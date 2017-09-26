da5c36c4898847b5bd7febac39a2050282e2a509
Annie Tang <annietang@berkeley.edu>
2017-09-26 13:38:17 -0700

    initializing repo

---
+++ mdb-socials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mdb-socials
+//
+//  Created by Annie Tang on 9/26/17.
+//  Copyright © 2017 Annie Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
