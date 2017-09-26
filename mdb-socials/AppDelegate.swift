5712d5f5c63509fb4d293d336c000978f94f2b27
Annie Tang <annietang@berkeley.edu>
2017-09-26 13:58:41 -0700

    starting loginviewcontroller

--- mdb-socials/AppDelegate.swift
+++ mdb-socials/AppDelegate.swift
@@ -13,6 +13,13 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+        -> Bool {
+            FirebaseApp.configure()
+            return true
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
--- mdb-socials/LoginViewController.swift
+++ mdb-socials/LoginViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    var titleLabel: UILabel!
+    var loginButton: UIButton!
+    var signupButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupTitleLabel()
+        setupLoginButton()
+        setupSignupButton()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupTitleLabel() {
+        titleLabel = UILabel(frame:
+            CGRect(x: view.frame.width * 0.2,
+                   y: view.frame.height * 0.05,
+                   width: view.frame.width * 0.7,
+                   height: view.frame.height * 0.3))
+        titleLabel.text = "MDB SOCIALS"
+        titleLabel.textColor = .black
+        titleLabel.font = UIFont(name:"Lato", size: 20)
+        titleLabel.layer.borderWidth = 1
+        titleLabel.layer.borderColor = UIColor.black.cgColor
     }
-
+    
+    func setupLoginButton() {
+        
+    }
+    
+    func setupSignupButton() {
+    
+    }
 
 }
 
