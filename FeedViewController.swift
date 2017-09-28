1193baf43c042a8abedcac8457d35eb481b07e4f
Annie Tang <annietang@berkeley.edu>
2017-09-28 07:37:52 -0700

    trying to do feedvc

--- FeedViewController.swift
+++ FeedViewController.swift
@@ -10,17 +10,142 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    var tableView: UITableView!
     
-    /* order of images in pokemonImages should be same as in results */
-    var allEvents: [Event]!
-    var eventToPass: Event!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
         
     }
+    
+    
+    var newEventView: UITextField!
+    var newEventButton: UIButton!
+    var tableView: UITableView!
+    var allEvents: [Event] = []
+    var eventToPass: Event!
+    
+    var auth = FIRAuth.auth()
+    var postsRef: FIRDatabaseReference = FIRDatabase.database().reference().child("Posts")
+    var storage: FIRStorageReference = FIRStorage.storage().reference()
+    var currentUser: User?
+    var navBar: UINavigationBar!
+    
+    
+    //For sample post
+    let sampleEvent = Event()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        activityIndicator.startAnimating()
+        allEvents.append(sampleEvent)
+        fetchUser {
+            self.fetchPosts() {
+                print("done")
+                if self.newEventView != nil {
+                    self.newEventView.removeFromSuperview()
+                }
+                self.setupNewEventView()
+                self.setupButton()
+                
+                self.setupTableView()()
+                self.setupNavBar()
+                
+                
+                
+                activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupNavBar() {
+        let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 0.2))
+        let navItem = UINavigationItem(title: "Feed");
+        let logOutButton = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(logOut))
+        navItem.rightBarButtonItem = logOutButton
+        navBar.setItems([navItem], animated: false)
+        self.view.addSubview(navBar)
+    }
+    
+    func logOut() {
+        print("logging out")
+        let firebaseAuth = FIRAuth.auth()
+        do {
+            try firebaseAuth?.signOut()
+            self.dismiss(animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        
+    }
+    
+    func setupNewEventView() {
+        newEventView = UITextField(frame: CGRect(x: 10, y: 10, width: UIScreen.main.bounds.width - 20, height: 0.3 * UIScreen.main.bounds.height))
+        newEventView.layoutIfNeeded()
+        newEventView.layer.shadowRadius = 2.0
+        newEventView.layer.masksToBounds = true
+        newEventView.placeholder = "Create a social event!"
+        view.addSubview(newEventView)
+    }
+    
+    func setupButton() {
+        newEventButton = UIButton(frame: CGRect(x: 10, y: newPostView.frame.maxY + 10, width: UIScreen.main.bounds.width - 20, height: 50))
+        newEventButton.setTitle("Add Event", for: .normal)
+        newEventButton.setTitleColor(UIColor.blue, for: .normal)
+        newEventButton.layoutIfNeeded()
+        newEventButton.layer.borderWidth = 2.0
+        newEventButton.layer.cornerRadius = 3.0
+        newEventButton.layer.borderColor = UIColor.blue.cgColor
+        newEventButton.layer.masksToBounds = true
+        newEventButton.addTarget(self, action: #selector(addNewEvent), for: .touchUpInside)
+        view.addSubview(newEventButton)
+    }
+    
+    func addNewEvent(sender: UIButton!) {
+        let desc = newEventView.text!
+        newEventView.removeFromSuperview()
+        let newEvent = ["desc": desc, "creator": currentUser?.name, "imageUrl": currentUser?.imageUrl, "creatorID": currentUser?.id] as [String : Any]
+        let key = postsRef.childByAutoId().key
+        let childUpdates = ["/\(key)/": newPost]
+        postsRef.updateChildValues(childUpdates)
+    }
+    
+    func fetchPosts(withBlock: @escaping () -> ()) {
+        //TODO: Implement a method to fetch posts with Firebase!
+        let ref = FIRDatabase.database().reference()
+        ref.child("Posts").observe(.childAdded, with: { (snapshot) in
+            let post = Post(id: snapshot.key, postDict: snapshot.value as! [String : Any]?)
+            self.posts.append(post)
+            
+            withBlock()
+        })
+    }
+    
+    func fetchUser(withBlock: @escaping () -> ()) {
+        //TODO: Implement a method to fetch posts with Firebase!
+        let ref = FIRDatabase.database().reference()
+        ref.child("Users").child((self.auth?.currentUser?.uid)!).observeSingleEvent(of: .value, with: { (snapshot) in
+            let user = User(id: snapshot.key, userDict: snapshot.value as! [String : Any]?)
+            self.currentUser = user
+            withBlock()
+            
+        })
+    }
+
     
     /* Initializing table view, and adding it to view */
     func setupTableView(){
